afed907b5bf9437af5625f122702f5471877c69b
Ignacio Rojas Aguilar <imoon521@gmail.com>
2021-01-10 15:50:10 -0600

    Lista de usuarios

--- src/app/users/pages/users/users.component.ts
+++ src/app/users/pages/users/users.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public users$: Observable<User[]>;
+  constructor(private afs: AngularFirestore) {
+    this.users$ = this.afs
+      .collection<User>('users', (ref) => {
+        return ref.orderBy('displayName', 'asc');
+      })
+      .valueChanges({ idField: 'id' });
   }
 
+  ngOnInit(): void {}
 }
