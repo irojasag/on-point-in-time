63bd4cfad5a5a0f4632eed26cbbf8a4ea6db63e4
Ignacio Rojas Aguilar <imoon521@gmail.com>
2021-01-10 20:55:58 -0600

    Login with facebook

--- src/app/users/pages/users/users.component.ts
+++ src/app/users/pages/users/users.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/models/user.model';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-users',
@@ -10,7 +11,7 @@ import { User } from 'src/app/models/user.model';
 })
 export class UsersComponent implements OnInit {
   public users$: Observable<User[]>;
-  constructor(private afs: AngularFirestore) {
+  constructor(public auth: AuthService, private afs: AngularFirestore) {
     this.users$ = this.afs
       .collection<User>('users', (ref) => {
         return ref.orderBy('displayName', 'asc');
