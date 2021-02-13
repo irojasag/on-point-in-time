fb8b7358bf2bf8cac53f0e28da993f9db7027249
Ignacio Rojas Aguilar <imoon521@gmail.com>
2021-02-12 20:41:42 -0600

    Add analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'on-point-in-time';
+  ngOnInit(): void {
+    firebase.analytics();
+  }
 }
