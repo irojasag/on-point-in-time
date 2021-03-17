46998ea3cb7ffafe2f56ddeabd4250beec5be782
Ignacio Rojas Aguilar <imoon521@gmail.com>
2021-03-16 23:44:35 -0600

    Block user implementation

---
+++ src/app/guards/lock.guard.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth/auth.service';
+import { map, tap } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LockGuard implements CanActivate {
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.auth.user$.pipe(
+      map((user) => {
+        const isUserSignedIn = !!user;
+        let condition = isUserSignedIn;
+        if (isUserSignedIn) {
+          condition = condition && !user.locked;
+          if (!condition) {
+            this.snackBar.open(
+              `Tu usuario ha sido bloqueado, contacta con el administador`,
+              'Ok',
+              {
+                duration: 6000,
+              }
+            );
+            this.auth.signOut();
+          }
+        }
+        return condition;
+      }),
+      tap((loggedIn) => {
+        if (!loggedIn) {
+          this.router.navigate(['/']);
+          return false;
+        }
+        return true;
+      })
+    );
+  }
+}
