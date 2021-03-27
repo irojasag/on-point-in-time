0794476432abca938f58236d5c6c8c20ea559056
joseadm <alonsodelm@gmail.com>
2021-03-27 17:37:23 -0600

    Include filter by user

--- src/app/users/pages/users/users.component.ts
+++ src/app/users/pages/users/users.component.ts
@@ -4,8 +4,8 @@ import { User } from 'src/app/models/user.model';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { UserUpdateBottomSheetComponent } from '../user-update-bottom-sheet/user-update-bottom-sheet.component';
-import { AngularFirestore } from '@angular/fire/firestore';
 import { UserService } from 'src/app/services/user/user.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
@@ -14,14 +14,24 @@ import { UserService } from 'src/app/services/user/user.service';
 })
 export class UsersComponent implements OnInit {
   public users$: Observable<User[]>;
+  public filteredUsers: User[];
+  public users: User[];
+  public filterValue: FormControl;
+
 
   constructor(public auth: AuthService, 
     private userService: UserService,     
     private bottomSheet: MatBottomSheet) {
     this.users$ = this.userService.users$;
+    this.filterValue = new FormControl();
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.users$.subscribe(users => {
+      this.users = users;
+      this.restoreFilteredUsers();
+    });
+  }
 
   public openBottomSheet(userInfo): void {
     this.bottomSheet.open(UserUpdateBottomSheetComponent, {
@@ -29,6 +39,18 @@ export class UsersComponent implements OnInit {
     });
   }
 
-  
+  private restoreFilteredUsers() {
+    this.filteredUsers = this.users;
+  }
+
+  public filterUser(): void {
+    if(this.filterValue.value === "") {
+      this.restoreFilteredUsers();
+    } 
+    else {
+      this.filteredUsers = this.users.filter(item => item.displayName.toLowerCase().includes(this.filterValue.value.toLowerCase()));
+    }
+  }
+
 }
 
