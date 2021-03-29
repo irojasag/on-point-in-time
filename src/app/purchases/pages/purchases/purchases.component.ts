d316a7779838a1b70ba92a08fe35b1fc602d27af
joseadm <alonsodelm@gmail.com>
2021-03-29 09:39:03 -0600

    Include purchases filter

--- src/app/purchases/pages/purchases/purchases.component.ts
+++ src/app/purchases/pages/purchases/purchases.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Purchase } from 'src/app/models/purchase.model';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { PurchaseBottomSheetComponent } from '../../components/purchase-bottom-sheet/purchase-bottom-sheet.component';
-import { UserService } from 'src/app/services/user/user.service';
 import { PurchaseService } from 'src/app/services/purchase/purchase.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-purchases',
@@ -14,14 +14,20 @@ import { PurchaseService } from 'src/app/services/purchase/purchase.service';
 })
 export class PurchasesComponent implements OnInit {
   public purchases$: Observable<Purchase[]>;
+  public filteredPurchases: Purchase[];
+  public purchases: Purchase[];
+  public filterValue: FormControl;
+  public subscriptions: Subscription;
   public isUserAdmin: boolean;
+
   constructor(
     public auth: AuthService,
     private bottomSheet: MatBottomSheet,
-    private userService: UserService,
     private purchasesService: PurchaseService
   ) {
     this.isUserAdmin = false;
+    this.filterValue = new FormControl();
+    this.subscriptions = new Subscription();
     this.auth.user$.subscribe((user) => {
       if (user) {
         this.isUserAdmin = user.isAdmin || user.isSuperAdmin;
@@ -29,8 +35,31 @@ export class PurchasesComponent implements OnInit {
         this.purchases$ = this.purchasesService.getAllPurchases(
           needsWhere ? user.uid : null
         );
+        this.subscriptions.add(this.handlePurchasesSubscription());
       }
     });
+  }
+
+  ngOnInit(): void {}
+
+  private handlePurchasesSubscription(): Subscription {
+    return this.purchases$.subscribe(purchases => {
+      this.purchases = purchases;
+      this.restoreFilteredPurchases();
+    });
+  }
+
+  private restoreFilteredPurchases() {
+    this.filteredPurchases = this.purchases;
+  }
+
+  public filterPurchase(): void {
+    if(this.filterValue.value === "") {
+      this.restoreFilteredPurchases();
+    } 
+    else {
+      this.filteredPurchases = this.purchases.filter(item => item.client.displayName.toLowerCase().includes(this.filterValue.value.toLowerCase()));
+    }
   }
 
   public openBottomSheet(purchase: Purchase): void {
@@ -39,5 +68,7 @@ export class PurchasesComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 }
