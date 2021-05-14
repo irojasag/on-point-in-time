8a8a66567607658cb5af5dc6a2012bf6e115b7aa
Ignacio Rojas Aguilar <imoon521@gmail.com>
2021-05-13 19:31:10 -0600

    WIP

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyDWJy_lDRvbRHacniApgFpikpRD0RNN3hk',
-    authDomain: 'onpointintimedev.firebaseapp.com',
-    projectId: 'onpointintimedev',
-    storageBucket: 'onpointintimedev.appspot.com',
-    messagingSenderId: '171170980169',
-    appId: '1:171170980169:web:3ac5a8b1987e6a3124ba98',
-    measurementId: 'G-TY84MNWZDS',
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
   },
 };
